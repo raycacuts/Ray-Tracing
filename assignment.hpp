db42f265787fcd17c5491670a31975d4947d22c2
raycacuts <76809909+raycacuts@users.noreply.github.com>
2021-04-27 14:34:50 -0700

    Add files via upload

---
+++ assignment.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <atlas/math/Math.hpp>
+#include <atlas/math/Ray.hpp>
+
+#include <fmt/printf.h>
+#include <stb_image.h>
+#include <stb_image_write.h>
+
+#include <vector>
+
+using namespace atlas;
+using Colour = math::Vector;
+
+
+void saveToBMP(std::string const& filename,
+               std::size_t width,
+               std::size_t height,
+               std::vector<Colour> const& image);
+
+// Your code here.
